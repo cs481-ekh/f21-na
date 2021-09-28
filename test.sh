179f877d2a5fd295330108b953f69f5269e35fcc
njravenna <70493128+njravenna@users.noreply.github.com>
2021-09-28 12:28:58 -0600

    CURR variable

--- test.sh
+++ test.sh
@@ -3,40 +3,40 @@ set -e
 
 # python version test
 echo = "python testing"
-CURR = "python3 --version"
+CURR = $(python3 --version)
 SUB = "python 3.6"
 
-if [["$CURR"] =~ .*"SUB".*]]; 
+if [["$CURR" =~ .*"SUB".*]]; 
 then
     echo 0
 fi
 
 # tensorflow version test
 echo = "tensorflow testing"
-CURR = "pip list | grep tensorflow"
+CURR = $(pip list | grep tensorflow)
 SUB = "tensorflow (1.13"
 
-if [["$CURR"] =~ .*"SUB".*]]; 
+if [["$CURR" =~ .*"SUB".*]]; 
 then
     echo 0
 fi
 
 ## pybind version test
 echo = "pybind testing"
-CURR = "pip list | grep pybind"
+CURR = $(pip list | grep pybind)
 SUB = "pybind (11"
 
-if [["$current_ver"] =~ .*"SUB".*]]; 
+if [["$CURR" =~ .*"SUB".*]]; 
 then
     echo 0
 fi
 
 ## ros
 echo = "ros testing"
-current_ver = "pip list | grep ros"
+CURR = $(pip list | grep ros)
 SUB = "ros"
 
-if [["$current_ver"] =~ .*"SUB".*]]; 
+if [["$CURR" =~ .*"SUB".*]]; 
 then
     echo 0
 fi
