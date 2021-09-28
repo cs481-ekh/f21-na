c51b2f90e5d6551da1f6caf87892e9bba0259bdb
Andre Murphy <42878649+FixedSpecialty@users.noreply.github.com>
2021-09-28 12:13:16 -0600

    testing caps variables

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 set -e
 
 # python version test
-current_ver = 'python3 --version'
+CURR = 'python3 --version'
 SUB = 'python 3.6'
 
-if [["$current_ver"] =~ .*"SUB".*]]; 
+if [["$CURR"] =~ .*"SUB".*]]; 
 then
     echo 0
 fi
 
 # tensorflow version test
-current_ver = 'pip list | grep tensorflow'
+CURR = 'pip list | grep tensorflow'
 SUB = 'tensorflow (1.13'
 
 if [["$current_ver"] =~ .*"SUB".*]]; 
